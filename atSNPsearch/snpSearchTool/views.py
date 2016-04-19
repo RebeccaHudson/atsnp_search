be707d9c9d70dd379de2ff6af122ca621b9c34c1
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-04-19 22:33:21 +0100

    Change index and details view into IndexView and DetailView and organize the other view methods.

--- atSNPsearch/snpSearchTool/views.py
+++ atSNPsearch/snpSearchTool/views.py
@@ -3,40 +3,22 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import ScoresRow
 from django.core.urlresolvers import reverse
+from django.views import generic
 
-#works..
-def index(request):
-  #accessing snp data from the database:
-  snp_list = ScoresRow.objects.order_by('-snpid')[:5]
-  output = ', '.join(s.snpid for s in snp_list)
-  context = { 'snp_list' : snp_list, 'output_str' : output  }
-  #the following 2 lines were replaced by 'render'
-  #template = loader.get_template('snpSearchTool/index.html')
-  #return HttpResponse(template.render(context, request))
-  return render(request, 'snpSearchTool/index.html', context)
+#uses a generic view
+class IndexView(generic.ListView):
+  template_name = 'snpSearchTool/index.html'
+  context_object_name = 'snp_list'
 
-#The snpid should be replaced by
-#all of the different search input controls that have been discussed.
-#FIRST ITERATION of this view.
+  def get_queryset(self):
+    return ScoresRow.objects.order_by('-snpid')[:5]
 
-
-#works
-def results(request, requested_snpid):
-  return HttpResponse("going to check out the results when you ask for %s" % requested_snpid) 
-
-
-
-#detail should show the detail about ONE ScoresRow identified by snpid.
-def detail(request, id_of_scoresrow):  #requested_snpid comes out of the url.
-  try:
-    snp_scores_row = ScoresRow.objects.get(id= id_of_scoresrow)
-  except (KeyError, ScoresRow.DoesNotExist):
-    raise Http404("Scores row for requested snpid does not exist")
-  return render(request, 'snpSearchTool/detail.html', 
-            { 'id_of_scoresrow' : id_of_scoresrow,
-             'snp_scores_row' : snp_scores_row,
-             'more_context'   : "More Context!", })
-            #return HttpResponse("detail for snpid: %s" % requested_snpid)
+#uses a generic view 
+#expects the url parameter to be a primary key called 'pk'
+class DetailView(generic.DetailView):
+  model = ScoresRow
+  template_name = 'snpSearchTool/detail.html'
+  context_variable_name = 'snp_scores_row'
 
 
 #renders some conrtrols that make other requests?
@@ -45,9 +27,8 @@ def controlset(request):
   return render(request, 'snpSearchTool/controlset.html', { 'all_scores_rows' : allRows }   )
 
 
-
 #used to handle form input for a lookup
-#maybe a cleaner way to do this?
+#redirect on successful lookup is apparently the thing to do.
 def lookup_one_snp(request ):
    #snp_scores_row = get_object_or_404(ScoresRow, snpid=requested_snpid) 
    snpid_in = request.POST['requested_snpid']
@@ -59,3 +40,29 @@ def lookup_one_snp(request ):
      #consider redisplying the control form.
    else:
      return HttpResponseRedirect(reverse('snpSearchTool:detail',  args=(id_to_use,)  ))
+
+def results(request, requested_snpid):
+  return HttpResponse("going to check out the results when you ask for %s" % requested_snpid) 
+
+"""  
+#detail should show the detail about ONE ScoresRow identified by snpid.
+def detail(request, id_of_scoresrow):  #requested_snpid comes out of the url.
+  try:
+    snp_scores_row = ScoresRow.objects.get(id= id_of_scoresrow)
+  except (KeyError, ScoresRow.DoesNotExist):
+    raise Http404("Scores row for requested snpid does not exist")
+  return render(request, 'snpSearchTool/detail.html', 
+            { 'id_of_scoresrow' : id_of_scoresrow,
+             'snp_scores_row' : snp_scores_row,
+             'more_context'   : "More Context!", })
+            #return HttpResponse("detail for snpid: %s" % requested_snpid)
+"""
+
+"""
+def index(request):
+  snp_list = ScoresRow.objects.order_by('-snpid')[:5]
+  output = ', '.join(s.snpid for s in snp_list)
+  context = { 'snp_list' : snp_list, 'output_str' : output  }
+  return render(request, 'snpSearchTool/index.html', context)
+
+"""
