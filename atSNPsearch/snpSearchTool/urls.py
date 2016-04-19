9dc7e52eb3303a7ba41d83ee890e73eff6660447
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-04-19 22:30:23 +0100

    Changed urls.py to use generic templates for IndexView and DetailView.

--- atSNPsearch/snpSearchTool/urls.py
+++ atSNPsearch/snpSearchTool/urls.py
@@ -8,16 +8,17 @@ app_name = 'snpSearchTool'
 #include at least one example of a valid matching url for each of these.
 urlpatterns = [
   #example: snpSearchTool/index
-  url(r'^$', views.index, name='index'),
+  #url(r'^$', views.index, name='index'),
+  url(r'^$', views.IndexView.as_view(), name='index'),  
 
   #example :snpSearchTool/results/3
   url(r'^results/(?P<requested_snpid>[0-9]*)', views.results, name='results'),
 
   #example :snpSearchTool/3/detail
-  url(r'^(?P<id_of_scoresrow>[0-9]*)/detail', views.detail , name='detail'),
+  #url(r'^(?P<id_of_scoresrow>[0-9]*)/detail', views.detail , name='detail'),
   #url(r'^(?P<id_of_requested_row>[0-9]*)/detail', views.detail , name='detail'),
-  
-
+  url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name = 'detail'), 
+ 
   #example: snpSearchTool/controlset
   url(r'^controlset$', views.controlset, name='controlset'),
 
