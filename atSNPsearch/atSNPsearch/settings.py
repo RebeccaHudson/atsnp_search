3c0b95cdcc24206c217e20f1e9e4e07642f9d9cb
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-04-21 00:39:07 +0100

    Change the SQLite3 file we connect to. Get some pipelining in place for this.

--- atSNPsearch/atSNPsearch/settings.py
+++ atSNPsearch/atSNPsearch/settings.py
@@ -78,7 +78,8 @@ WSGI_APPLICATION = 'atSNPsearch.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'motif_scores.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, 'motif_scores.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'actual-existing-data-motif_scores.sqlite3'),
     }
 }
 
